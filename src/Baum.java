0fe937ae815bc93d4429a75edc840e4a21f8732d
samuel.stroot <samuel.stroot@gmail.com>
2024-03-14 09:49:25 +0100

    jürgen

--- src/Baum.java
+++ src/Baum.java
@@ -1,14 +1,25 @@
 public class Baum {
-    BinaryTree<String> bt;
+    BinaryTree<String> b7
 
     public Baum(){
-        bt= new BinaryTree<String>();
+
         BinaryTree<String> b1=new BinaryTree<String>("Jürgen");
         BinaryTree<String> b2=new BinaryTree<String>("Sibille");
         BinaryTree<String> b3=new BinaryTree<String>("Jason Jerome", b1,b2);
         BinaryTree<String> b4=new BinaryTree<String>("Anette");
         BinaryTree<String> b5=new BinaryTree<String>("Dieter" );
         BinaryTree<String> b6=new BinaryTree<String>("Eywa", b4,b5);
-        BinaryTree<String> b7=new BinaryTree<String>("Jeremy Pascal", b3,b6);
+       b7=new BinaryTree<String>("Jeremy Pascal", b3,b6);
+    }
+    public void preorder(){
+
+    }
+    public void besuche(BinaryTree<String> b ){
+        if(b7.getLeftTree()==null){
+            b7.getRightTree();
+
+
+        }
+
     }
 }
